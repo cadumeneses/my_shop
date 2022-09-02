fa8f693169200872b96e5d79d1a65c26230b003a
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-09-02 11:01:22 -0300

    feat: add grid view

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/data/dummy_data.dart';
+
+import '../models/product.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  const ProductsOverviewPage({super.key});
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Shop'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          itemBuilder: ((context, index) => Text(loadedProducts[index].title)),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ),
+      ),
+    );
   }
 }
