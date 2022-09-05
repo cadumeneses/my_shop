62ab8f7c66de12fa0aaa6b93beeb14412af17450
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-09-05 12:40:53 -0300

    style: page order

--- lib/components/order.dart
+++ lib/components/order.dart
@@ -2,22 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:my_shop/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
   const OrderWidget({required this.order, super.key});
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: () {},
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (!_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: (widget.order.products.length * 25) + 10,
+              child: ListView(
+                children: widget.order.products.map(
+                  (product) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          product.title,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '${product.quantity}x ${product.price}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ).toList(),
+              ),
+            ),
+        ],
       ),
     );
   }
