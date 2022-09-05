cd2c95fe0a24e80ea9306727e3dff68bc3a3eb5c
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-09-05 12:03:22 -0300

    style: order page

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/components/app_drawer.dart';
+import 'package:my_shop/components/order.dart';
+import 'package:my_shop/models/order_list.dart';
+import 'package:provider/provider.dart';
 
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('My orders'),
+      ),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: orders.itemsCount,
+        itemBuilder: (ctx, index) => OrderWidget(order: orders.items[index]),
       ),
     );
   }
